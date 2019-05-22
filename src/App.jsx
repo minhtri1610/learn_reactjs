16f74becbe1ba9323279073992363a9016628c91
Tran Minh Tri <tran_minh_tri@management-partners.co.jp>
2019-05-22 10:57:23 +0700

    update reactjs forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,28 @@ class App extends React.Component {
     constructor(pops){
         super(pops);
         this.state = {
-            data: 0
+            data: 'data state'
         }
-        this.setNewNumber = this.setNewNumber.bind(this)
+        this.updateState = this.updateStateAbc.bind(this);
     };
-    setNewNumber(){
-        this.setState({data: this.state.data + 1})
+    updateStateAbc(e){
+        this.setState({data: e.target.value})
     }
     render(){
         return(
             <div>
-                <button onClick = {this.setNewNumber}>INCREMENT</button>
-                <Content myNumber = {this.state.data}></Content>
+                <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
             </div>
         )
     }
 }
 
 class Content extends React.Component{
-    componentWillMount(){
-        alert('1 will mount');
-    }
-    componentDidMount(){
-        alert('2 did mount');
-    }
-    componentWillReceiveProps(nextProps){
-        alert('3 will receive props');
-        // console.log('3' + nextProps);
-    }
-    shouldComponentUpdate(nextProps, nextState){
-        return true;
-    }
-    componentWillUpdate(nextProps, nextState){
-        alert('4 ');
-    }
-    componentDidUpdate(prevProps, prevState){
-        alert(5);
-    }
-    componentWillUnmount() {
-        alert(6);
-    }
     render(){
         return(
             <div>
-                <h3>{this.props.myNumber}</h3>
+                <input type="text" value = {this.props.myDataProp} onChange = {this.props.updateStateProp}/>
+                <h3>{this.props.myDataProp}</h3>
             </div>
         )
     }
